1e158ea8d8e7b20940065f6456ed57d119c664b6
Anuj <anujkandari623@gmail.com>
2026-09-03 15:31:56 +0530

    fix js

--- js/script.js
+++ js/script.js
@@ -4,63 +4,77 @@ let taskList = document.getElementById("taskList");
 
 
 let CompletedCount = document.getElementById("CompletedCount");
-let uncompletedCount =document.getElementById("uncompletedCount");
+let uncompletedCount = document.getElementById("uncompletedCount");
 
 
-addBtn.addEventListener("click" , function() {
- 
+addBtn.addEventListener("click", function () {
+
     let task = taskInput.value;
-   if(task === ""){
-    return;
-   }
+    if (task === "") {
+        return;
+    }
 
-   taskList.innerHTML  += `
-   <div class ="border rounded p-2 mb-2 d-flex  align-item-center">
+    taskList.innerHTML += `
+   <div class ="border rounded p-3 mb-3 d-flex  align-items-center">
    <input type ="checkbox" class ="form-check-input me-3" onchange ="completeTask(this)">
-   <span class =flex-grow-1 task-text me-3 ">
+   <span class ="flex-grow-1 task-text me-3 ">
       ${task}
       </span>
 
-      <button class ="btn  btn-sm btn-danger me-2">
+      <button class ="btn  btn-sm btn-danger me-2" onclick = "deleteTask(this)">
       Delete
       </button>
 
-      <button class = "btn btn-sm btn-warning">
+      <button class = "btn btn-sm btn-warning" onclick = "editTask(this)">
       Edit
       </button>
 
    </div>
    `;
-   taskInput.value = "";
-   updateCount();
+    taskInput.value = "";
+    updateCount();
 });
 
-function completeTask(checkbox){
+function completeTask(checkbox) {
     let taskText = checkbox.nextElementSibling;
 
-    if(checkbox.checked){
+    if (checkbox.checked) {
         taskText.style.textDecoration = "line-through";
         taskText.style.color = "gray";
-    } else{
-        taskText.style.textDecoration ="none";
-        taskText.style.color ="black";
+    } else {
+        taskText.style.textDecoration = "none";
+        taskText.style.color = "black";
     }
-     updateCount();
+    updateCount();
 }
 
-function updateCount(){
-    let allTask =taskList.querySelectorAll(".form-check-input");
+function updateCount() {
+    let allTask = taskList.querySelectorAll(".form-check-input");
 
     let completed = 0;
     let uncompleted = 0;
 
-    allTasks.forEach(function(checkbox){
-        if (checkbox.checked){
+    allTask.forEach(function (checkbox) {
+        if (checkbox.checked) {
             completed++;
-        } else{
+        } else {
             uncompleted++;
         }
     });
     CompletedCount.textContent = completed;
     uncompletedCount.textContent = uncompleted;
+}
+
+function deleteTask(button) {
+    button.parentElement.remove();
+    updateCount();
+}
+
+function editTask(button) {
+    let taskText = button.parentElement.querySelector(".task-text");
+    let newTask = prompt("Edit your task:", taskText.textContent.trim());
+
+    if (newTask !== null && newTask.trim() !== "") {
+        taskText.textContent = newTask;
+    }
 }
